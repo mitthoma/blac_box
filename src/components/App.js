f1339b8cdcf08f4dc228c6581440d93a0ab83a64
Mitchell Thomas <mt@almuts-MacBook.localdomain>
2021-04-05 17:03:13 -0400

    finished app

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-//import DStorage from '../abis/DStorage.json'
+import DStorage from '../abis/DStorage.json'
 import React, { Component } from 'react';
 import Navbar from './Navbar'
 import Main from './Main'
@@ -6,6 +6,9 @@ import Web3 from 'web3';
 import './App.css';
 
 //Declare IPFS
+
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https'})
 
 class App extends Component {
 
@@ -39,37 +42,79 @@ class App extends Component {
     this.setState({ account: accounts[0] });
 
     //Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DStorage.networks[networkId]
+    if(networkData) {
+      const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+      this.setState({ dstorage })
 
-    //IF got connection, get data from contracts
-      //Assign contract
+      const filesCount = await dstorage.methods.fileCount().call()
+      this.setState({ dstorage })
 
-      //Get files amount
+      for (var i = filesCount; i >= 1; i--) {
+        const file = await dstorage.methods.files(i).call()
+        this.setState({
+          files: [...this.state.files, file]
+        })
+      }
+    } else {
+      window.alert('DStorage contract not deployed to detected network')
+    }
 
-      //Load files&sort by the newest
-
-    //Else
-      //alert Error
+    this.setState({ loading: false })
 
   }
 
   // Get file from user
   captureFile = event => {
+
+    event.preventDefault()
+
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result),
+        type: file.type,
+        name: file.name
+      })
+      console.log('buffer', this.state.buffer)
+    }
   }
 
 
   //Upload File
   uploadFile = description => {
 
-    //Add file to the IPFS
+    console.log("Submitting file to IPFS...")
 
-      //Check If error
-        //Return error
+    // Add file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('IPFS result', result.size)
+      if(error) {
+        console.error(error)
+        return
+      }
 
-      //Set state to loading
-
-      //Assign value for the file without extension
-
-      //Call smart contract uploadFile function
+      this.setState({ loading: true })
+      // Assign value for the file without extension
+      if(this.state.type === ''){
+        this.setState({type: 'none'})
+      }
+      this.state.dstorage.methods.uploadFile(result[0].hash, result[0].size, this.state.type, this.state.name, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({
+         loading: false,
+         type: null,
+         name: null
+       })
+       window.location.reload()
+      }).on('error', (e) =>{
+        window.alert('Error')
+        this.setState({loading: false})
+      })
+    })
 
   }
 
@@ -77,7 +122,12 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      account: '',
+      dstorage: null,
+      files: [],
+      loading: false,
+      type: null,
+      name: null
     }
 
     //Bind functions
